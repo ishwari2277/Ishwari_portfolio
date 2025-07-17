5ecfc0a0237df5ae4fc4d142f0fb41c05631a460
Ishwari Sangle <147730218+ishwari2277@users.noreply.github.com>
2025-07-17 12:14:46 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaLocationArrow } from "react-icons/fa6";
-
-import { MagicButton } from "@/components/ui/magic-button";
 import { links } from "@/config";
 import { socialMedia } from "@/data";
 
